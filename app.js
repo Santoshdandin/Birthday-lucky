62255757c31a1af53b67434a1e556c59d43de146
Santoshdandin <santusb427@gmail.com>
2022-01-31 23:23:10 +0530

    addeventlistener

---
+++ app.js
@@ -0,0 +1,9 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+
+
+
+checkNumberButton.addEventListener("click", function getValues(){
+    
+})
