6d8c0b347729281ecfb45b085b2186dc84f8c96c
Santoshdandin <santusb427@gmail.com>
2022-02-01 00:41:19 +0530

    calculation

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const checkNumberButton = document.querySelector("#check-number");
 
 
 
-checkNumberButton.addEventListener("click", function getValues(){
-    
-})
+function checkBirthDateIsLucky() {
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    console.log(sum);
+}
+
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for(let i=0; i<dob.length; i++) {
+        sum = sum + Number(dob.charAt(i));
+    }
+
+    return sum;
+}
+
+checkNumberButton.addEventListener("click", checkBirthDateIsLucky);
