07548d0aa3af49e67d95f5a9ff3726efa3a0f39f
Santoshdandin <santusb427@gmail.com>
2022-02-01 01:59:18 +0530

    lucky or unlucky

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
+
+
+function compareValues(sum, numberToCheck) {
+    if(sum % numberToCheck ===0 ) {
+       outputBox.innerText = "your birthday is lucky 🎉";
+    }else {
+        outputBox.innerText = "your birthday is not lucky 😞";
+    }
+}
 
 
 
 function checkBirthDateIsLucky() {
     const dob = dateOfBirth.value;
-    const sum = calculateSum(dob);
-    console.log(sum);
+    const numberToCheck = luckyNumber.value
+    if(dob && numberToCheck) {
+    compareValues(sum, numberToCheck)
+    } else {
+    outputBox.innerText = "Please enter both fields";
+    }
 }
 
 
 function calculateSum(dob) {
     dob = dob.replaceAll("-", "");
     let sum = 0;
-    for(let i=0; i<dob.length; i++) {
-        sum = sum + Number(dob.charAt(i));
-    }
+    for(let index=0; index<dob.length; index++) {
+        sum = sum + Number(dob.charAt(index));
+    };
 
     return sum;
-}
+};
 
 checkNumberButton.addEventListener("click", checkBirthDateIsLucky);
